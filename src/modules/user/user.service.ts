4d6e9a0b79bc4b004cf11a52758addd00a78062d
hckrmoon <hckrmoon@gmail.com>
2017-11-10 08:48:32 +0900

    typo fixed

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,19 +1,24 @@
 import {Component, Inject} from "@nestjs/common";
-import {} from "./user.provider";
-import {UsersRepository} from "./user.provider";
 import {Model} from "sequelize-typescript";
 import {User} from "./user.entity";
 import {CreateUserDto} from "./dto/create-user.dto";
+import {UsersRepository} from "./user.provider";
 
 @Component()
 export class UserService {
   constructor(@Inject(UsersRepository) private readonly userRepository: typeof Model) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    return this.userRepository.create(createUserDto)
+    const user = new User()
+
+    user.email = createUserDto.email
+    user.username = createUserDto.username
+    user.password = createUserDto.password
+
+    return await user.save()
   }
 
   async findAll(): Promise<User[]> {
-    return this.userRepository.findAll<User>()
+    return await this.userRepository.findAll<User>()
   }
 }
