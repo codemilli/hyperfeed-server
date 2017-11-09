d2d31a4f465c25be03098eb71f0301d31286d17f
hckrmoon <hckrmoon@gmail.com>
2017-11-10 08:55:21 +0900

    sequelize debugging

--- src/modules/database/database.provider.ts
+++ src/modules/database/database.provider.ts
@@ -4,20 +4,21 @@ import {User} from "../user/user.entity";
 
 export const SequelizeToken = 'SequelizeToken'
 export const databaseProviders = [
-    {
-        provide: SequelizeToken,
-        useFactory: async () => {
-            const sequelize = new Sequelize({
-                dialect: 'mysql',
-                host: ENV.DB_HOST,
-                port: ENV.DB_PORT,
-                username: ENV.DB_USER,
-                password: ENV.DB_PASS,
-                database: ENV.DB_NAME,
-            })
-            sequelize.addModels([User])
-            await sequelize.sync()
-            return sequelize
-        }
+  {
+    provide: SequelizeToken,
+    useFactory: async () => {
+      console.log('use sequelize factory')
+      const sequelize = new Sequelize({
+        dialect: 'mysql',
+        host: ENV.DB_HOST,
+        port: ENV.DB_PORT,
+        username: ENV.DB_USER,
+        password: ENV.DB_PASS,
+        database: ENV.DB_NAME,
+      })
+      sequelize.addModels([User])
+      await sequelize.sync()
+      return sequelize
     }
+  }
 ]
