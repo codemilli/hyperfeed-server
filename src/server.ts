25011041dd6d81480363e5463125f266c1c13862
hckrmoon <hckrmoon@gmail.com>
2017-11-13 23:56:26 +0900

    cors module added

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import * as bodyParser from 'body-parser'
+import * as cors from 'cors'
 import {NestFactory} from '@nestjs/core'
 import {ApplicationModule} from './modules/app.module'
 
@@ -6,7 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule)
   app.use(bodyParser.urlencoded({extended: false}))
   app.use(bodyParser.json())
-  await app.listen(3000)
+  app.use(cors())
+  await app.listen(3100)
 }
 
 bootstrap()
